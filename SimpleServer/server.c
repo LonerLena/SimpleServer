7e3d82b68c0f960b2be4205e9dd770c8965a714c
LonerLena <lonerlena@pm.me>
2020-04-23 12:27:29 +0200

    Created function: start, accept. Added more documentation.

--- SimpleServer/server.c
+++ SimpleServer/server.c
@@ -2,11 +2,61 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#define PORT 1337
-#define BUFFER 4096
+
+int s, conn;
+struct sockaddr_in server, client;
+
+/**
+ * Starts the TCP server.
+ * @param int port, port for server to run on.
+ * @return int 0 on success.
+ */
+int start(int port) {
+	s = socket(AF_INET, SOCK_STREAM, 0);
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	server.sin_port = htons(port);
+	if (bind(s, (struct sockaddr*)&server, sizeof(server)) != 0) {
+		perror("bind");
+		return(1);
+	}
+	if(listen(s, 6) != 0) {
+		perror("listen");
+		return(2);
+	}
+	return(0);
+}
+
+/**
+ * Accepts an incoming connection.
+ * @return connection.
+ */
+int acceptConnection() {
+	conn = accept(s, (struct sockaddr*)&client, sizeof(client));
+	if(conn < 0) {
+		return -1;
+	}
+	return conn;
+}
+
+int * getClients() {
+	return 0;
+}
+
+//TODO: Ban an IP address.
+int ban() {
+	return 0;
+}
+
+//TODO: Kick a client.
+int kick() {
+	return 0;
+}
 
 /**
  * Send message to client.
+ * @param int conn, connection.
+ * @param char msg, message.
  */
 int sendMessage(int conn, char msg) {
 	if(write(conn, msg, sizeof(msg)) != 0) {
@@ -17,56 +67,18 @@ int sendMessage(int conn, char msg) {
 
 /**
  * Send message to all clients.
+ * @param int index, total amount of clients.
+ * @param char msg, message.
  */
 int broadcast(int clients[], int index, char msg) {
 	for(int i = 0; i < index; i++) {
 		sendMessage(clients[i], msg);
 	}
-
 	return 0;
 }
 
-void receiveMessage() {
-	while(1) {
-	}
+//TODO: Receive messages.
+char * receiveMessage() {
+	return("");
 }
 
-int main() {
-	int clients[256];
-	int clientsIndex = 0;
-	int s, conn, totalClients;
-	struct sockaddr_in client, server;
-	
-	printf("Starting server...\n");
-	
-	s = socket(AF_INET, SOCK_STREAM, 0);
-
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = htonl(INADDR_ANY);
-	server.sin_port = htons(PORT);
-
-	if (bind(s, (struct sockaddr*)&server, sizeof(server)) != 0){
-		perror("bind");
-		exit(0);
-	}
-
-	if(listen(s, 6) != 0) {
-		perror("listen");
-		exit(0);
-	}
-
-	printf("Server started successfully!\n");
-	totalClients = sizeof(client);
-	while(1) {
-		conn = accept(s, (struct sockaddr*)&client, &totalClients);
-		if(conn < 0) {
-			perror("accept");
-		}
-		printf("User connected: %d!\n",conn);
-		int clientsLength = sizeof(clients)/sizeof(*clients);
-		clients[clientsIndex] = conn;
-		clientsIndex++;
-	}
-
-	return 0;
-}
